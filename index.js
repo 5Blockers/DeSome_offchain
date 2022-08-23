051f624fa5f20414a1d9e54b144a9afd833458a2
Ngo Viet Tien <viettien1602@gmail.com>
2022-08-23 19:51:23 +0700

    add socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const socket = require('socket.io')
 const cors = require('cors')
 const app = express()
 
@@ -28,3 +29,34 @@ const PORT = process.env.PORT || 5000
 const server = app.listen(PORT, () => {
     console.log(`Listenning on port ${PORT}...`)
 })
+
+//socket
+const io = socket(server, {
+    cors: {
+      origin: process.env.FRONT_END_URL,
+      credentials: true,
+    }
+});
+  
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+
+    socket.on("add-user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
+  
+    socket.on('send-message', (data) => {
+      const sendUserSocket = onlineUsers.get(data.to);
+      if (sendUserSocket) {
+        io.to(sendUserSocket).emit('msg-receive', data.message);
+      }
+    });
+
+    socket.on('check-online', (userId) => {
+        const currentUser = onlineUsers.get(userId)
+        let msg = false
+        if (currentUser) msg = true
+        io.to(currentUser).emit('is-online', msg)
+    })
+});
